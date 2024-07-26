f949422eea0495b8aa781268b7117607239865cb
Andres Fernandez <46586316+andyf9903@users.noreply.github.com>
2024-07-25 21:57:18 -0400

    reload window when header clicked

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,15 @@
+"use client"
 import Link from "next/link"
 
 export default function Header() {
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    window.location.reload()
+  }
     return (
     <nav className="flex justify-center py-4 bg-slate-900 text-white">
-        <Link className="text-lg font-bold text-blue-400 hover:text-blue-300" href="/"> Weather App </Link>
+        <Link className="text-lg font-bold text-blue-400 hover:text-blue-300" href="/" onClick={handleClick}> Weather App </Link>
       </nav>
     )
 }
